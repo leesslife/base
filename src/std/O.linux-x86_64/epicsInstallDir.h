2a88220a1a7f141b19dcf58d296984f91e19bfb5
root <root@localhost.localdomain>
2020-07-06 23:43:42 -0400

    base0.01

---
+++ src/std/O.linux-x86_64/epicsInstallDir.h
@@ -0,0 +1,8 @@
+/* THIS IS A GENERATED FILE. DO NOT EDIT! */
+
+#ifndef INC_epicsInstallDir_H
+#define INC_epicsInstallDir_H
+
+#define EPICS_BASE "/home/lees2/base-3.15.5"
+
+#endif /* INC_epicsInstallDir_H */
